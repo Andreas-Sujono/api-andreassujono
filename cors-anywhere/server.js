616bd6ca1c48479a22b99bcfd364c873c5439a1c
AndreasSujono <andreassujono@dathena.io>
2021-04-14 23:56:20 +0800

    update config

--- cors-anywhere/server.js
+++ cors-anywhere/server.js
@@ -1,7 +1,7 @@
 const cors_proxy = require('cors-anywhere');
 
 // Listen on a specific host via the HOST environment variable
-const host = process.env.HOST || '127.0.0.1';
+const host = process.env.HOST || '0.0.0.0';
 
 // Listen on a specific port via the PORT environment variable
 const port = process.env.PORT || 8080;
